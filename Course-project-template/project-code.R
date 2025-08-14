fc85e3af8f98d4696580bca430038c5712f95721
Jenine Harris <harrisj@wustl.edu>
2025-08-14 16:54:06 -0500

    Add files via upload

---
+++ Course-project-template/project-code.R
@@ -0,0 +1,58 @@
+# packages for the project
+
+# import the data
+
+# data management
+# can included selecting variables, cleaning variable names,
+# adding labels, recoding, other cleaning or management processes
+
+
+# check your work using summary()
+
+
+# check assumptions for any continuous variables
+
+
+# create table 1 of descriptive statistics
+
+
+# create figure 1 
+# outcome + predictor variable 1
+
+
+# find appropriate group statistics for outcome + predictor variable 1 
+# (group means & sd -OR- group medians & IQR -OR- group frequencies and percentages)
+# check assumptions if needed
+
+
+# create figure 2
+# outcome + one of the predictor variables
+
+
+# find appropriate group statistics for outcome + predictor variable 1 
+# (group means & sd -OR- group medians & IQR -OR- group frequencies and percentages)
+# check assumptions if needed
+
+
+
+# create figure 3
+# outcome + one of the predictor variables
+
+
+# find appropriate group statistics for outcome + predictor variable 1 
+# (group means & sd -OR- group medians & IQR -OR- group frequencies and percentages)
+# check assumptions if needed
+
+
+
+
+# for ***at least one*** of the predictors conduct the appropriate bivariate
+# inferential test (one of these: chi-squared, t-test, ANOVA)
+# check assumptions
+# report  and interpret assumption checking results and inferential test results on the poster
+
+
+# additional code for the policy brief
+# can include other data sources or other analyses
+
+
